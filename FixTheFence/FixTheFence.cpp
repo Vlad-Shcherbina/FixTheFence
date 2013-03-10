685d472a171fb290772e7d7153254a9e118caace
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-03-10 20:01:46 +0400

    path tracing

--- FixTheFence/FixTheFence.cpp
+++ FixTheFence/FixTheFence.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
+#include <sstream>
 
 using namespace std;
 
@@ -10,19 +12,33 @@ const int STRIDE = 256;
 const int MAX_SIZE = 100;
 const int NO_GOAL = 5;
 
+const int UP = -STRIDE;
+const int DOWN = STRIDE;
+const int LEFT = -1;
+const int RIGHT = 1;
+
+
 bool m[STRIDE*(MAX_SIZE+2)] = {false};
 int goal[STRIDE*(MAX_SIZE+2)];
 
 
+typedef pair<int, int> Coords;
+
+
 class Block {
 	int offset;
-	int w, h;
 
 public:
+	int w, h;
+
 	Block(int offset, int w, int h) : offset(offset), w(w), h(h) {}
 
 	inline int coords_to_index(int x, int y) {
 		return offset + x + STRIDE*y;
+	}
+
+	inline Coords index_to_coords(int pt) {
+		return Coords(pt % STRIDE - offset % STRIDE, pt / STRIDE - offset / STRIDE);
 	}
 
 	void show() {
@@ -43,6 +59,49 @@ public:
 			cerr << endl;
 		}
 	}
+
+	string trace_path(int pt, int prev_d, Coords &finish) {
+		int start = pt;
+
+		string path;
+		int d = prev_d;
+
+		Coords xy;
+
+		while (true) {
+			bool ul = m[pt-STRIDE-1];
+			bool ur = m[pt-STRIDE];
+			bool dl = m[pt-1];
+			bool dr = m[pt];
+
+			assert(ul == ur || ur == dr || dr == dl);
+
+			if (ul != ur && d != DOWN) {
+				d = UP;
+				path.push_back('U');
+			} else if (dl != dr && d != UP) {
+				d = DOWN;
+				path.push_back('D');
+			} else if (ul != dl && d != RIGHT) {
+				d = LEFT;
+				path.push_back('L');
+			} else if (ur != dr && d != LEFT) {
+				d = RIGHT;
+				path.push_back('R');
+			} else
+				assert(false);
+
+			pt += d;
+			xy = index_to_coords(pt);
+			if (pt == start)
+				break;
+
+			if (xy.first < 0 || xy.first > w || xy.second < 0 || xy.second > h)
+				break;
+		}
+		finish = xy;
+		return path;
+	}
 };
 
 
@@ -52,6 +111,7 @@ void clear() {
 		goal[i] = NO_GOAL;
 	}
 }
+
 
 class FixTheFence {
 public:
@@ -67,7 +127,27 @@ public:
 				else
 					goal[pt] = c - '0';
 			}
+
+		for (int y = 0; y < whole.h; y++) {
+			int pt = whole.coords_to_index(whole.w/2, y);
+			m[pt] = true;
+		}
+
 		whole.show();
+
+		for (int pt = 0; ; pt++)
+			if (m[pt]) {
+				Coords start = whole.index_to_coords(pt+1);
+				Coords finish;
+				string path = whole.trace_path(pt+1, RIGHT, finish);
+				assert(finish == start);
+
+				ostringstream out;
+				out << start.second << " " << start.first << " " << path;
+				return out.str();
+			}
+
+		cerr << "FAIL" << endl;
 		return "0 0 RDLU";
 	}
 };
