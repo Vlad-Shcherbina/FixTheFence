44f81ddedc63741491df07654113abaf7b6b113e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-03-10 19:00:50 +0400

    add symmetry check

--- dynamic.py
+++ dynamic.py
@@ -31,6 +31,54 @@ if __name__ == '__main__':
 
     sys.stdout = sys.stderr
     seed = 109
+
+
+    while True:
+        print '------------'
+        random.seed(seed)
+        print 'seed', seed
+        seed += 1
+
+        whole = Block.make_empty(10, 9)
+
+        for _ in range(30):
+            for pt in whole.enum_points():
+                if whole.can_change(pt) and random.random() < 0.5:
+                    whole.change(pt)
+
+
+        params = 0.5, 1, 1, 1, 0.5
+        randomize_block_goal(whole.get_subblock(0, 0, whole.w, whole.h), *params)
+
+        block = whole.get_subblock(0, 2, whole.w, whole.h-2)
+
+        checked_dynamic(block)
+
+        for x in range(-1, whole.w//2):
+            for y in range(-1, whole.h+1):
+                pt1 = whole.coords_to_index(x, y)
+                pt2 = whole.coords_to_index(whole.w-1-x, y)
+                whole.m[pt1], whole.m[pt2] = whole.m[pt2], whole.m[pt1]
+                whole.goal[pt1], whole.goal[pt2] = whole.goal[pt2], whole.goal[pt1]
+
+        r = dynamic(block, sameness_bonus=0.001)
+        assert len(r) == 0, r
+
+        for x in range(-1, whole.w):
+            for y in range(-1, whole.h//2):
+                pt1 = whole.coords_to_index(x, y)
+                pt2 = whole.coords_to_index(x, whole.h-1-y)
+                whole.m[pt1], whole.m[pt2] = whole.m[pt2], whole.m[pt1]
+                whole.goal[pt1], whole.goal[pt2] = whole.goal[pt2], whole.goal[pt1]
+
+        r = dynamic(block, sameness_bonus=0.001)
+        assert len(r) == 0, r
+
+        #whole.show()
+        #raw_input()
+
+
+
     while True:
         print '------------'
         random.seed(seed)
